f682ccf7e7fecc3f39ae3e32d873f3f21e1bbba3
shukrullo <shukrulloqurbonov342@gmail.com>
2026-03-25 23:33:34 +0500

    add adding task functionality

--- app/src/main/java/com/example/todotaskmanager/MainActivity.kt
+++ app/src/main/java/com/example/todotaskmanager/MainActivity.kt
@@ -4,13 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+
+import androidx.compose.ui.unit.dp
 import com.example.todotaskmanager.ui.theme.TodoTaskManagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,21 +36,50 @@ class MainActivity : ComponentActivity() {
         setContent {
             TodoTaskManagerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
+                    var tasks by remember { mutableStateOf(listOf<String>()) }
+                    var taskText by remember { mutableStateOf("")}
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                            .padding(16.dp)
+                    ) {
+                        Text(
+                            text = "Tasks",
+                            style = MaterialTheme.typography.headlineMedium
+                        )
+
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        OutlinedTextField(
+                            value = taskText,
+                            onValueChange = { taskText = it },
+                            label = { Text("Enter task") },
+                            modifier = Modifier.fillMaxWidth()
+                        )
+
+                        Spacer(modifier = Modifier.height(16.dp))
+                        LazyColumn(
+                            verticalArrangement = Arrangement.spacedBy(8.dp)
+                        ) {
+                            items(tasks) { task ->
+                                Text(text = task)
+                            }
+                        }
+                        Button(onClick = {
+                            if(taskText.isNotBlank()){
+                                tasks += taskText
+                                taskText = ""
+                            }
+                        }) {
+                            Text(text = "Add Task")
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
